af07b524e5276c8ce786566df54ce3ccfe4dc03d
Shubham <shubhamlahase@hmail.com>
2023-07-11 18:14:30 +0530

    added catregories and conact component on homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Home.scss";
 import Slider from "../../components/slider/Slider";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 
 const Home = () => {
     return(
@@ -9,6 +11,8 @@ const Home = () => {
             <Slider/>
             <FeaturedProducts type="featured"/>
             <FeaturedProducts type="trending"/>
+            <Categories/>
+            <Contact/>
         </div>
     );
 };
