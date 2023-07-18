1f76cc91e9d47e874f6b7212b38f8c5142b7375d
Shubham <shubhamlahase@hmail.com>
2023-07-18 17:44:11 +0530

    single product page right side

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,28 +1,75 @@
-import React, { useState } from "react"
-import "./Product.scss"
+import React, { useState } from "react";
+import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
-    const [selectedImg, setSelectedImg] =  useState(0);
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
 
-    const images = [
-        "https://images.pexels.com/photos/11201681/pexels-photo-11201681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        "https://images.pexels.com/photos/11201731/pexels-photo-11201731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-    ]
+  const images = [
+    "https://images.pexels.com/photos/11201681/pexels-photo-11201681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/11201731/pexels-photo-11201731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  ];
 
-    return(
-        <div className="product">
-            <div className="left">
-                <div className="images">
-                    <img src={images[0]} alt="" onClick={(e)=>setSelectedImg(0)} />
-                    <img src={images[1]} alt="" onClick={(e)=>setSelectedImg(1)}/>
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$189</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <div className="quantity">
+            <button onClick={() => setQuantity((prev)=>prev === 1 ? 1 : prev - 1)}>-</button>
+            {quantity}
+            <button onClick={() => setQuantity((prev)=>prev+1)}>+</button>
+        </div>
+        <div>
+            <button className="add">
+                <AddShoppingCartIcon/> ADD TO CART
+            </button>
+            <div className="link">
+                <div className="item">
+                    <FavoriteBorderIcon/> ADD TO WISHLIST
                 </div>
-                <div className="mainImg">
-                    <img src={images[selectedImg]} alt="" />
+                <div className="item">
+                    <BalanceIcon/> ADD TO COMPARE
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Product;
