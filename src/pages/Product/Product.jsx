ed74a46d207655b8739c8de6185313e1afd5d179
Shubham <shubhamlahase@hmail.com>
2023-07-18 16:32:41 +0530

    single product page left side

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react"
+import "./Product.scss"
 
 const Product = () => {
+    const [selectedImg, setSelectedImg] =  useState(0);
+
+    const images = [
+        "https://images.pexels.com/photos/11201681/pexels-photo-11201681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "https://images.pexels.com/photos/11201731/pexels-photo-11201731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    ]
+
     return(
-        <>Product</>
-    );
-};
+        <div className="product">
+            <div className="left">
+                <div className="images">
+                    <img src={images[0]} alt="" onClick={(e)=>setSelectedImg(0)} />
+                    <img src={images[1]} alt="" onClick={(e)=>setSelectedImg(1)}/>
+                </div>
+                <div className="mainImg">
+                    <img src={images[selectedImg]} alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
+    )
+}
 
 export default Product;
